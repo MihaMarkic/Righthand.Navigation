206578690419ea4ed4242b508a00f9cc27ca7a52
Miha Markič <miha@rthand.com>
2018-03-17 23:24:10 +0100

    Adds blank iOS Sample

---
+++ src/Righthand.Navigation/Sample/Righthand.Navigation.Sample.iOS/MainViewController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Righthand.Navigation.Sample.ViewModels;
+using UIKit;
+
+namespace Righthand.Navigation.Sample.iOS
+{
+    public partial class MainViewController : UINavigationController
+    {
+        readonly MasterViewModel viewModel;
+        public MainViewController(MasterViewModel viewModel)
+        {
+            this.viewModel = viewModel;
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            var text = new UITextView { Text = "No page" };
+            Add(text);
+        }
+    }
+}
